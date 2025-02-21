2ff628b09ac5ffc0b4b2f200a50cb25aa31da514
Gabriel Natan <gabriel--natan@live.com>
2025-02-20 23:49:14 -0300

    feat(AppTopbar): change color

--- src/components/app-topbar/index.tsx
+++ src/components/app-topbar/index.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export function AppTopbar() {
   return (
-    <header className="flex justify-end flex-1 h-16 shrink-0 items-center gap-2 border-b px-4 max-h-16">
+    <header className="flex bg-white justify-end flex-1 h-16 shrink-0 items-center gap-2 border-b px-4 max-h-16">
       <Avatar>
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
         <AvatarFallback>CN</AvatarFallback>
