71be596925c24e178190fa43519a844fb83af66f
Gabriel Natan <gabriel--natan@live.com>
2025-03-01 16:15:29 -0300

    feat(pages): add CreateAccount page

---
+++ src/pages/CreateAccount/index.tsx
@@ -0,0 +1,148 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+const formSchema = z.object({
+  first_name: z.string(),
+  last_name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  confirm_password: z.string(),
+});
+
+export const CreateAccount = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full max-w-xl space-y-4"
+      >
+        <div className="w-full  flex gap-5 items-center">
+          <FormField
+            control={form.control}
+            name="first_name"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="w-full"
+                    placeholder="Digite seu Nome"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last_name"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>Sobrenome</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="w-full"
+                    placeholder="Digite seu sobrenome"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  className="w-full"
+                  placeholder="Digite seu Email"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Senha</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  className="w-full"
+                  placeholder="Digite seu senha"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="confirm_password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirme Sua Senha</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  className="w-full"
+                  placeholder="Confirme sua senha"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-full" type="submit">
+          Login
+        </Button>
+        <Link to="/auth/login">Go to login</Link>
+      </form>
+    </Form>
+  );
+};
