e7a241ecad8f5d9ac4624a9104581307f2519a7c
Gabriel Natan <gabriel--natan@live.com>
2025-03-01 16:14:41 -0300

    feat(routes): create a new route for CreateAccount

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { AuthLayout } from "./layout/auth";
+import { CreateAccount } from "./pages/CreateAccount";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
       <Routes>
         <Route Component={AuthLayout}>
           <Route path="/auth/login" Component={Login} />
+          <Route path="/auth/create-account" Component={CreateAccount} />
         </Route>
         <Route path="/" Component={Home} />
       </Routes>
